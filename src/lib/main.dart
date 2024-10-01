2c6bba55ac9f27cda6c5a4edd89ea86f9f8a7e5c
zharfan-a <zharfan@walturn.com>
2024-10-01 07:59:50 +0700

    Update 15 files

---
+++ src/lib/main.dart
@@ -0,0 +1,30 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'screens/login_screen.dart';
+import 'screens/home_screen.dart';
+import 'cubits/auth_cubit.dart';
+
+void main() {
+	runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+	@override
+	Widget build(BuildContext context) {
+		return MultiBlocProvider(
+			providers: [
+				BlocProvider<AuthCubit>(
+					create: (context) => AuthCubit(),
+				),
+			],
+			child: MaterialApp(
+				title: 'Simple App',
+				theme: ThemeData(
+					primarySwatch: Colors.blue,
+				),
+				home: LoginScreen(),
+			),
+		);
+	}
+}
